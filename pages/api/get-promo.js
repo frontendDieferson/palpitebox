81c35b9df6211e1a237c71de0e8f39319a31d139
Dieferson Soares <62387982+frontendDieferson@users.noreply.github.com>
2020-07-05 16:47:07 -0400

    fix

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -3,8 +3,8 @@ import { GoogleSpreadsheet } from 'google-spreadsheet'
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 // fix encode
 const fromBase64 = value => {
-  const buff = Buffer.from(value, 'base64');
-  return buff.toString('ascii');
+  const buff = Buffer.from(value, 'base64')
+  return buff.toString('ascii')
 }
 
 export default async (req, res) => {
