da87165cc7124dea92e2b921be4d485fa512df82
Dieferson Soares <62387982+frontendDieferson@users.noreply.github.com>
2020-07-05 17:01:45 -0400

    fix stable

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,7 +1,7 @@
-
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 import moment from 'moment'
 import { fromBase64 } from '../../utils/base64'
+
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
 const genCupom = () => {
