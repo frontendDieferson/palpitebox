5d771314211cf267e8ddef4801495be90fb51edc
Dieferson <diefersonfrontenddeveloper@gmail.com>
2020-10-07 17:33:47 -0400

    Fix Front-end

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,9 +4,10 @@ const Footer = () => {
   return (
     <div className='bg-green-700 p-4'>
       <div className='container mx-auto text-center font-bold text-white'>
-      Projeto desenvolvido por: Dieferson Soares  <a className='hover:underline' href='https://github.com/frontendDieferson/'>  / Github </a>
+      Projeto desenvolvido por: Dieferson Soares  <a className='hover:underline' 
+      href='https://github.com/frontendDieferson/'>/ Github </a>
         <div className='mt-4'>
-          <img className='inline' src='/twoDevslogo.png' width='150' height='90' />
+          <img className='inline' src='/happyCode.png' width='150' height='90' />
         </div>
       </div>
     </div>
